4ee4ebec592c3c137c98a7f89e402bf83072514f
Your Name <you@example.com>
2025-09-06 15:05:20 -0700

    ur code highkey sucks im workin on it but don wanna rn

--- frontend/static/annotation.js
+++ frontend/static/annotation.js
@@ -1,125 +1,128 @@
 const canvas = document.getElementById("annotation");
 const ctx = canvas.getContext("2d");
 
-let img = new Image;
+let img = new Image();
 let framenum = 0;
 
-img.onload = function() {
-    ctx.drawImage(img, 0, 0);
-}
-img.src = '/frame?ts=' + new Date().getTime();
+img.onload = function () {
+  ctx.drawImage(img, 0, 0);
+};
+img.src = "/frame?ts=" + new Date().getTime();
 
-fetch('/framenum').then(resp => resp.json()).then(data => {
+fetch("/framenum")
+  .then((resp) => resp.json())
+  .then((data) => {
     framenum = data;
-});
+  });
 
 colors = ["red", "orange", "yellow", "green", "blue", "violet"];
 
 function Dot(i) {
-    this.x = i * 32 + 400;
-    this.y = 20;
-    this.r = 6;
-    this.color = colors[i];
-    this.dragging = false;
+  this.x = i * 32 + 400;
+  this.y = 20;
+  this.r = 6;
+  this.color = colors[i];
+  this.dragging = false;
 }
 
 let dots = [];
 for (var i = 0; i < 6; i++) {
-    dots.push(new Dot(i));
+  dots.push(new Dot(i));
 }
 
 let dragging = false;
 
 function draw() {
-    ctx.drawImage(img, 0, 0);
+  ctx.drawImage(img, 0, 0);
 
-    for (var i = 0; i < dots.length; i++) {
-        ctx.beginPath();
-        ctx.arc(dots[i].x, dots[i].y, dots[i].r, 0, Math.PI * 2);
-        ctx.fillStyle = dots[i].color;
-        ctx.fill();
-        ctx.closePath();
-    }
+  for (var i = 0; i < dots.length; i++) {
+    ctx.beginPath();
+    ctx.arc(dots[i].x, dots[i].y, dots[i].r, 0, Math.PI * 2);
+    ctx.fillStyle = dots[i].color;
+    ctx.fill();
+    ctx.closePath();
+  }
 }
 
 function nextframe() {
-    const goodframe = document.getElementById("goodframe").checked;
-    let teams = [];
+  const goodframe = document.getElementById("goodframe").checked;
+  let teams = [];
 
-    for (var i = 1; i < 7; i++) {
-        teams.push([
-            document.getElementById(`team${i}-good`).checked,
-            document.getElementById(`team${i}-num`).value
-        ])
-    }
+  for (var i = 1; i < 7; i++) {
+    teams.push([
+      document.getElementById(`team${i}-good`).checked,
+      document.getElementById(`team${i}-num`).value,
+    ]);
+  }
 
-    let data = {
-        goodframe: `${goodframe}`,
-        teams: `${teams}`,
-        dots: [],
-        framenum: `${framenum}`
-    };
+  let data = {
+    goodframe: `${goodframe}`,
+    teams: `${teams}`,
+    dots: [],
+    framenum: `${framenum}`,
+  };
 
-    for (var i = 0; i < dots.length; i++) {
-        data.dots.push({ x: dots[i].x, y: dots[i].y });
-    }
+  for (var i = 0; i < dots.length; i++) {
+    data.dots.push({ x: dots[i].x, y: dots[i].y });
+  }
 
-    fetch("/data", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
+  fetch("/data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
     })
-    .then(response => response.json())
-    .then(result => {
-        console.log(result);
-    })
-    .catch(error => {
-        console.log(error);
-    })
-
-    img.src = '/frame?ts=' + new Date().getTime();
-    fetch('/framenum').then(resp => resp.json()).then(data => {
-        framenum = data;
+    .catch((error) => {
+      console.log(error);
     });
 
-    draw();
+  img.src = "/frame?ts=" + new Date().getTime();
+  fetch("/framenum")
+    .then((resp) => resp.json())
+    .then((data) => {
+      framenum = data;
+    });
+
+  draw();
 }
 
 function mouseOnDot(dotx, doty, dotr, x, y) {
-    let dx = x - dotx;
-    let dy = y - doty;
-    let dist2 = dx * dx + dy * dy;
-    return dist2 <= dotr * dotr;
+  let dx = x - dotx;
+  let dy = y - doty;
+  let dist2 = dx * dx + dy * dy;
+  return dist2 <= dotr * dotr;
 }
 
-canvas.addEventListener("mousedown", e => {
-    dragging = true;
+canvas.addEventListener("mousedown", (e) => {
+  dragging = true;
 });
 
-canvas.addEventListener("mousemove", e => {
-    if (dragging) {
-        const rect = canvas.getBoundingClientRect();
-        let x = e.clientX - rect.left;
-        let y = e.clientY - rect.top;
-
-        for (var i = 0; i < dots.length; i++) {
-            if (mouseOnDot(dots[i].x, dots[i].y, dots[i].r, x, y) || dots[i].dragging) {
-                dots[i].x = Math.max(dots[i].r, Math.min(canvas.width - dots[i].r, x));
-                dots[i].y = Math.max(dots[i].r, Math.min(canvas.height - dots[i].r, y));
-                dots[i].dragging = true;
-            }
-        }
-
-        draw();
-    } else {
-        for (var i = 0; i < dots.length; i++) {
-            dots[i].dragging = false;
-        }
+canvas.addEventListener("mousemove", (e) => {
+  if (!dragging) {
+    for (var i = 0; i < dots.length; i++) {
+      dots[i].dragging = false;
     }
+    return;
+  }
+  const rect = canvas.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+
+  for (var i = 0; i < dots.length; i++) {
+    if (mouseOnDot(dots[i].x, dots[i].y, dots[i].r, x, y) || dots[i].dragging) {
+      dots[i].x = Math.max(dots[i].r, Math.min(canvas.width - dots[i].r, x));
+      dots[i].y = Math.max(dots[i].r, Math.min(canvas.height - dots[i].r, y));
+      dots[i].dragging = true;
+    }
+  }
+  draw();
 });
 
 canvas.addEventListener("mouseup", () => {
-    dragging = false;
+  dragging = false;
 });
